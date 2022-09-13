86ce60df181ae337d6199bc4a8317c379f3e0732
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-09-13 14:00:08 +0530

    functions

---
+++ file.js
@@ -0,0 +1,50 @@
+// here we will try to understand the difference beytweeen a normal function and an arrow function
+
+//Normal function
+function archit (){
+
+    console.log (Hey this is a normal function)
+}
+
+archit();
+
+//This is also a way to write function
+
+const archit = function (){
+
+
+    console.log (Hey this is also a normal function)
+}
+
+archit ();
+
+
+// Arrow function
+
+const archit = ()=> (){
+
+
+    console.log (This is an arrow function)
+
+}
+archit ();
+
+//another example
+"one liners wont require braces/return"
+""instead of ""
+
+
+
+const greet = function (){
+return "Goood Morning"
+}
+
+
+"" we can write ""
+
+
+ const greet = ()=> "Good Morning";
+ console.log (greet())
+
+
+
