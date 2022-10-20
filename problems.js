e4345fd056ffa0eac4c229270f8ce0438bdd5929
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-10-20 14:57:19 +0530

    more added

--- problems.js
+++ problems.js
@@ -14,3 +14,21 @@ console.log(sol);
 function removeChar(str) {
   return str.slice(1, -1);
 }
+
+//bullets and dragons solution
+// Each dragon takes two bullets
+// True, False tells about survival of hunter
+function hero(bullets, dragons) {
+  if (bullets >= dragons * 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Multiplication by 8
+// we need to check if number is even we have to multiply it by 8 else by 9
+
+function simpleMultiplication(num) {
+  return num * (num % 2 ? 9 : 8);
+}
